7cdb536512ed44adcb8e87448e2ffd62ef45bd99
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-10 00:29:07 +0600

    added custom  status codes

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status
 from .database import engine, SessionLocal
 from . import schemas, models
 from sqlalchemy.orm import Session
@@ -15,10 +15,22 @@ def get_db():
         db.close()
 
 
-@app.post("/blog")
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title = request.title, body = request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.get("/blog", status_code=status.HTTP_200_OK)
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+@app.get("/blog/{id}", status_code=status.HTTP_200_OK)
+def show(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    return blog
+
