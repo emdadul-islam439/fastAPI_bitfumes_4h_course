6e4efda9876bca498d6c34d60d165812e601e6a9
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-09 17:51:54 +0600

    Written our first POST API after creating the DB

--- blog/database.py
+++ blog/database.py
@@ -6,6 +6,6 @@ from sqlalchemy.orm import sessionmaker
 SQL_ALCHEMY_DATABASE_URL = 'sqlite:///./blog.db'
 engine = create_engine(SQL_ALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
-Session = sessionmaker(bind=engine, autocommit=False, autoflush=False,)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False,)
 
 Base = declarative_base()
