ac2027d2d24b25f1f6dd19c18af73273424ca7f4
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-09 07:04:44 +0600

    WRONG: placing '/blog/unpublished' after '/blog/{id}' will lead to error, because 'id' is int

--- main.py
+++ main.py
@@ -6,13 +6,20 @@ app = FastAPI()
 def index():
     return { "Data" : "Index Page" }
 
+
 @app.get("/blog/{id}")
 def show(id: int):
     return { "Data" : f"Blog no. {id}" }    
+
 
 @app.get("/blog/{id}/comments")
 def comments(id: int):
     return { 
         "ID" : id,
         "Comments" : ["Comment 1", "Comment 2"] 
-        }
+    }
+
+
+@app.get("/blog/unplublished")
+def unpublished():
+    return { "Data" : "Some unpublished blogs" }
