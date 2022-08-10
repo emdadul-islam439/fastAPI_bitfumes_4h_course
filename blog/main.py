2c59f7811420699f0692e1414b074510924d7ead
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-10 18:21:02 +0600

    successfully created delete() and put() APIs

--- blog/main.py
+++ blog/main.py
@@ -1,3 +1,4 @@
+from distutils.util import execute
 from fastapi import FastAPI, Depends, status, HTTPException
 from .database import engine, SessionLocal
 from . import schemas, models
@@ -22,6 +23,42 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id no. {id} is not found!")
+
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return "Done"
+
+
+@app.put("/blog/{id}", status_code= status.HTTP_202_ACCEPTED)
+def update(id: int, request: schemas.UpdatedBlog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id no. {id} not found!")
+    
+    updated_request = {}
+    if request.title:
+        updated_request['title'] = request.title
+    if request.body:
+        updated_request['body'] = request.body
+
+    # raw code
+    # blog.update({"title" : "updated title", "body": "updated body"})  
+
+    # blog.update(request.dict()) will not be good choice, request.dict() updates all 'not given' field as 'null'
+    # But we want every not given field as usual
+    blog.update(updated_request)
+
+    db.commit()
+    return  "Updated!"
 
 
 @app.get("/blog", status_code=status.HTTP_200_OK)
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,5 +1,12 @@
+# from typing import Optional
+from typing import Optional
 from pydantic import BaseModel
 
 class Blog(BaseModel):
     title: str
     body: str
+
+
+class UpdatedBlog(BaseModel):
+    title: Optional[str] = None
+    body: Optional[str] = None
