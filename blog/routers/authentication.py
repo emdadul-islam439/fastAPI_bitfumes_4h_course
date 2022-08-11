80e25cafc2dd8ee39de29ab3ab08cb03d6a7fc04
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-12 03:37:19 +0600

    successfully generated JWT access token.

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, status, HTTPException, Depends
-from .. import schemas, database, models
+from .. import schemas, database, models, token
 from sqlalchemy.orm import Session
 from ..hashing import Hash
 
@@ -18,6 +18,5 @@ def login(request: schemas.Login, db: Session = Depends(database.get_db)):
     if not Hash.verify(request.password , user.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Given password is not correct!")
 
-    # Generate JWT token here and RETURN it
-
-    return user
+    access_token = token.create_access_token(data={"sub": user.email})
+    return {"access_token": access_token, "token_type": "bearer"}
