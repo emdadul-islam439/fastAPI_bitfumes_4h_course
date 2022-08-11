0cb9fcc6a0c1030668a476dbafb0f4a51dc9f75f
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-12 03:12:29 +0600

    checked 'hashed' password with user given password

--- blog/hashing.py
+++ blog/hashing.py
@@ -5,3 +5,6 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 class Hash():
     def get_password_hash(password: str):
         return pwd_context.hash(password)
+    
+    def verify(plain_password, hashed_password):
+        return pwd_context.verify(plain_password, hashed_password)
--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,10 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, HTTPException, Depends
+from .. import schemas, database, models
+from sqlalchemy.orm import Session
+from ..hashing import Hash
 
 router = APIRouter(
     tags = ['Authentication'],
     prefix="/login"
 )
 
-@router.get("/")
-def login():
-    return 'login'
+@router.post("/", status_code=status.HTTP_202_ACCEPTED)
+def login(request: schemas.Login, db: Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.email == request.username).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with username = {request.username} not found!")
+
+    if not Hash.verify(request.password , user.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Given password is not correct!")
+
+    # Generate JWT token here and RETURN it
+
+    return user
