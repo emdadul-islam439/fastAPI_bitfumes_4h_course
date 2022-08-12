dcb9cb2ad4af2130025c74e1bc5f14074310aa9a
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-12 23:00:02 +0600

    re-structured the files again

--- blog/database.py
+++ blog/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQL_ALCHEMY_DATABASE_URL = 'sqlite:///./blog/blog.db'
+SQL_ALCHEMY_DATABASE_URL = 'sqlite:///./blog.db'
 engine = create_engine(SQL_ALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False,)
